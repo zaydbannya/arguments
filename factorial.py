6917e30305a5c3b0ad354027fe9a6766d5363f4a
zaydbannya <zayd.bannya@gmail.com>
2025-01-29 13:16:18 +0100

    factorial

---
+++ factorial.py
@@ -0,0 +1,12 @@
+def factorial(x):
+    '''this is a recuring function ti find the factorial of intiger'''
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return x*factorial (x-1)
+print(factorial.__doc__)
+print("the factorial of 0:",factorial(0))
+print("the factorial of 1:",factorial(1))
+print("the factorial of 2:",factorial(2))
+print("the factorial of 5:",factorial(5))
+print("the factorial of 10:",factorial(10))
