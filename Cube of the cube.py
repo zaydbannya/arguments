614fc3b638285d721115a7e9297cadbcb0c68b8f
zaydbannya <zayd.bannya@gmail.com>
2025-01-29 12:58:36 +0100

    cube of cube

---
+++ Cube of the cube.py
@@ -0,0 +1,9 @@
+def cube (number):
+    return number*number*number
+def by_three (number):
+    if number %3 == 0:
+        return cube (number)
+    else:
+        return False
+print(by_three(9))
+print(by_three(4))
